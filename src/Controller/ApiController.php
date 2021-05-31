45fbf95727e0efc740b9c4553a9c086460f98138
Leo Cormoreche <leocormo@gmail.com>
2021-05-31 18:53:36 +0200

    Gestion calendrier avec fullCalendar.js

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Calendar;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api", name="api")
+     */
+    public function index(): Response
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+
+    /**
+     * @Route("/api/{id}/edit", name="api_event_edit", methods={"PUT"})
+     */
+    public function majEvent(?Calendar $calendar, Request $request): Response
+    {
+
+        // On recupere les données
+        $donnees = json_decode($request->getContent());
+
+        if(
+            isset($donnees->title) && !empty($donnees->title) &&
+            isset($donnees->start) && !empty($donnees->start) &&
+            isset($donnees->end) && !empty($donnees->end) &&
+            isset($donnees->description) && !empty($donnees->description) &&
+            isset($donnees->backgroundColor) && !empty($donnees->backgroundColor) &&
+            isset($donnees->textColor) && !empty($donnees->textColor)
+        ){
+            // les données sont complètes
+            // On initialise un code 200
+            $code = 200;
+
+            // On verifie si l'id existe
+            if(!$calendar){
+                // On instancie un rdv
+                $calendar = new Calendar;
+
+                // On change le code
+                $code = 201;
+            }
+
+            // On hydrate l'objet avec les données
+            $calendar->setTitle($donnees->title);
+            $calendar->setDescription($donnees->description);
+            $calendar->setStart(new DateTime($donnees->start));
+            $calendar->setEnd(new DateTime($donnees->end));
+            $calendar->setBackgroundColor($donnees->backgroundColor);
+            $calendar->setTextColor($donnees->textColor);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($calendar);
+            $em->flush();
+
+            // On retourne un code
+            return new Response('Ok ', $code);
+        }else{
+            // les données sont incomplètes
+            return new Response('Données incomplètes', 404);
+        }
+
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+}
