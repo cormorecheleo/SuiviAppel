9f289cde2bae72d337ffd649f80ae6f2c6b4e512
Leo Cormoreche <leocormo@gmail.com>
2021-05-31 08:23:59 +0200

    Redirection apres connexion reussi

--- src/Security/LoginFormAuthenticatorTypeAuthenticator.php
+++ src/Security/LoginFormAuthenticatorTypeAuthenticator.php
@@ -96,7 +96,8 @@ class LoginFormAuthenticatorTypeAuthenticator extends AbstractFormLoginAuthentic
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('welcome'));
     }
 
     protected function getLoginUrl()
